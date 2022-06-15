92a5d6893863a8e8e55b43242fd1f598e2827946
Pablo Ríos <pablo@appicua.com>
2022-06-15 15:41:22 -0500

    commit 2

--- src/script.js
+++ src/script.js
@@ -10,3 +10,34 @@ botones.forEach(btn => {
         console.log(destino)
     })
 })
+
+const pops = document.querySelectorAll('.pieza')
+pops.forEach(i => {
+    i.addEventListener('click', (event) => {
+        event.preventDefault()
+        const destino = i.dataset.modal
+        console.log(destino)
+        const modal = document.getElementById(destino)
+        modal.classList.add("mostrar")
+        
+    })
+})
+
+
+const btncerrar = document.querySelectorAll('.cerrar')
+
+
+ var parentDiv = btncerrar.parentNode;
+
+
+
+btncerrar.forEach(a => {
+    a.addEventListener('click', (event) => {
+        event.preventDefault()
+        var parentDiv = a.closest('.overlay');
+        parentDiv.classList.remove("mostrar")
+        console.log(parentDiv) 
+
+              
+    })
+})
