050cf4681952045034e17e53f36b7df91af2bf14
Juan Orjuela <juan@appicua.com>
2022-06-15 15:45:12 -0500

    preMerge

--- src/script.js
+++ src/script.js
@@ -6,7 +6,21 @@ const botones = document.querySelectorAll('.btn')
 botones.forEach(btn => {
     btn.addEventListener('click', (event) => {
         event.preventDefault()
-        const destino = btn.dataset.destino
-        console.log(destino)
+        const destino = document.getElementById(btn.dataset.destino)
+        const origen = document.getElementById(btn.dataset.origen)
+        
+        origen.classList.add('saliendo')
+        destino.classList.add('entrando')
+
+        setTimeout(() => {
+            origen.classList.remove('saliendo')
+            origen.classList.remove('activo')
+            destino.classList.remove('entrando')
+            destino.classList.add('activo')
+        }, 4000);
+
     })
 })
+
+//Temores
+const temores = []
